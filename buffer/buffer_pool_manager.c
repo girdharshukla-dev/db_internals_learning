a91c58b1bef2813306109b689b850210f115cc7c
Girdhar Shukla <girdharshukla26@gmail.com>
2026-04-16 20:55:14 +0530

    buffer wip

--- buffer/buffer_pool_manager.c
+++ buffer/buffer_pool_manager.c
@@ -41,16 +41,20 @@ struct db_buffer_pool* buffer_init(int pool_size, int fd){
 
   // pthread_mutex_init(&bp->latch, NULL);
 
-  bp->frames = malloc(sizeof(bp->pool_size * sizeof(struct page_table_entry)));
+  bp->frames = malloc(bp->pool_size * sizeof(struct db_frame));
   for(int i = 0; i < pool_size; i++){
-    bp->frames[i].page_id = 0;
+    bp->frames[i].page_id = -1;
     bp->frames[i].pin_count = 0;
     bp->frames[i].is_dirty = 0; 
   }
 
-  bp->page_entry = malloc(sizeof(bp->pool_size * sizeof(struct page_table_entry)));
+  bp->page_entry = malloc(bp->pool_size * sizeof(struct page_table_entry));
   for(int i = 0; i < bp->pool_size; i++){
     bp->page_entry[i].page_id = -1;
+  }
+  bp->free_list = malloc(pool_size * sizeof(int));
+  for(size_t i = 0; i < pool_size; i++){
+    bp->free_list[i] = i;
   }
   bp->free_count = bp->pool_size;
   return bp;
@@ -66,7 +70,13 @@ int pt_lookup(struct db_buffer_pool *bp, int page_id){
 }
 
 void pt_insert(struct db_buffer_pool *bp, int page_id, int frame_id){
-  
+  for(size_t i = 0; i < bp->pool_size; i++){
+    if(bp->page_entry[i].page_id == -1){
+      bp->page_entry[i].page_id = page_id;
+      bp->page_entry[i].frame_id = frame_id;
+      break;
+    }
+  }
 }
 
 int main() {
