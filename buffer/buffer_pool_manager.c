a61e8ee582b245352bf6d18f1a1c2da199f4e03d
Girdhar Shukla <girdharshukla26@gmail.com>
2026-04-17 20:09:39 +0530

    naive eviction

--- buffer/buffer_pool_manager.c
+++ buffer/buffer_pool_manager.c
@@ -1,40 +1,10 @@
+#include "buffer_pool_manager.h"
+
 #include <stdlib.h>
 #include <pthread.h>
 
-#define PAGE_SIZE 4096
-
-struct db_frame {
-  int page_id;
-  int pin_count;
-  int is_dirty;
-  char data[PAGE_SIZE];
-};
-
-struct page_table_entry{
-int frame_id;
-int page_id;
-};
-
-struct db_buffer_pool {
-  struct db_frame *frames;
-  int pool_size;
-
-  struct page_table_entry* page_entry;
-  
-  int *free_list;
-  int free_count;
-
-  int *lru_list;
-  int lru_size;
-
-  // pthread_mutex_t latch;
-
-  int next_page_id;
-  int fd;
-};
-
-struct db_buffer_pool* buffer_init(int pool_size, int fd){
-  struct db_buffer_pool* bp = malloc(sizeof(struct db_buffer_pool));
+struct db_buffer_pool *buffer_init(int pool_size, int fd) {
+  struct db_buffer_pool *bp = malloc(sizeof(struct db_buffer_pool));
   bp->pool_size = pool_size;
   bp->fd = fd;
   bp->next_page_id = 0;
@@ -42,36 +12,36 @@ struct db_buffer_pool* buffer_init(int pool_size, int fd){
   // pthread_mutex_init(&bp->latch, NULL);
 
   bp->frames = malloc(bp->pool_size * sizeof(struct db_frame));
-  for(int i = 0; i < pool_size; i++){
+  for (int i = 0; i < pool_size; i++) {
     bp->frames[i].page_id = -1;
     bp->frames[i].pin_count = 0;
-    bp->frames[i].is_dirty = 0; 
+    bp->frames[i].is_dirty = 0;
   }
 
   bp->page_entry = malloc(bp->pool_size * sizeof(struct page_table_entry));
-  for(int i = 0; i < bp->pool_size; i++){
+  for (int i = 0; i < bp->pool_size; i++) {
     bp->page_entry[i].page_id = -1;
   }
   bp->free_list = malloc(pool_size * sizeof(int));
-  for(size_t i = 0; i < pool_size; i++){
+  for (size_t i = 0; i < pool_size; i++) {
     bp->free_list[i] = i;
   }
   bp->free_count = bp->pool_size;
   return bp;
 }
 
-int pt_lookup(struct db_buffer_pool *bp, int page_id){
-  for(size_t i = 0; i < bp->pool_size; i++){
-    if(bp->page_entry[i].page_id == page_id){
+int pt_lookup(struct db_buffer_pool *bp, int page_id) {
+  for (size_t i = 0; i < bp->pool_size; i++) {
+    if (bp->page_entry[i].page_id == page_id) {
       return bp->page_entry[i].frame_id;
     }
   }
   return -1;
 }
 
-void pt_insert(struct db_buffer_pool *bp, int page_id, int frame_id){
-  for(size_t i = 0; i < bp->pool_size; i++){
-    if(bp->page_entry[i].page_id == -1){
+void pt_insert(struct db_buffer_pool *bp, int page_id, int frame_id) {
+  for (size_t i = 0; i < bp->pool_size; i++) {
+    if (bp->page_entry[i].page_id == -1) {
       bp->page_entry[i].page_id = page_id;
       bp->page_entry[i].frame_id = frame_id;
       break;
@@ -79,19 +49,35 @@ void pt_insert(struct db_buffer_pool *bp, int page_id, int frame_id){
   }
 }
 
-struct db_frame* fetch_page(struct db_buffer_pool* bp, int page_id){
+struct db_frame *fetch_page(struct db_buffer_pool *bp, int page_id) {
   int frame_id = pt_lookup(bp, page_id);
-  if(frame_id != -1){
+  if (frame_id != -1) {
     struct db_frame *frame = &bp->frames[frame_id];
     frame->pin_count += 1;
     return frame;
   }
 
-  if(bp->free_count == 0){
-    return NULL;
+  int new_frame_id;
+
+  if (bp->free_count == 0) {
+    int victim_frame_id = find_victim(bp);
+    if (victim_frame_id == -1) {
+      return NULL;
+    }
+    if(bp->frames[victim_frame_id].is_dirty == 1){
+      //write to disk
+    }
+    for (size_t j = 0; j < bp->pool_size; j++) {
+      if (bp->page_entry[j].frame_id == victim_frame_id) {
+        bp->page_entry[j].page_id = -1;
+        bp->page_entry[j].frame_id = -1;
+      }
+    }
+    new_frame_id = victim_frame_id;
+  }else{
+    new_frame_id = bp->free_list[--bp->free_count];
   }
 
-  int new_frame_id = bp->free_list[--bp->free_count];
   struct db_frame *frame = &bp->frames[new_frame_id];
   frame->page_id = page_id;
   frame->pin_count = 1;
@@ -101,18 +87,22 @@ struct db_frame* fetch_page(struct db_buffer_pool* bp, int page_id){
   return frame;
 }
 
-void unpin_page(struct db_buffer_pool *bp, int page_id, int is_dirty){
+void unpin_page(struct db_buffer_pool *bp, int page_id, int is_dirty) {
   int frame_id = pt_lookup(bp, page_id);
-  if(frame_id != -1){
+  if (frame_id != -1) {
     bp->frames[frame_id].pin_count -= 1;
-    if(is_dirty){
+    if (is_dirty) {
       bp->frames[frame_id].is_dirty = 1;
     }
   }
 }
 
-
-int main() {
-
-  return 0;
+int find_victim(struct db_buffer_pool *bp) {
+  for (size_t i = 0; i < bp->pool_size; i++) {
+    if (bp->frames[i].pin_count == 0) {
+      return i;
+    }
+  }
+  return -1;
 }
+
---
+++ buffer/buffer_pool_manager.h
@@ -0,0 +1,44 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#define PAGE_SIZE 4096
+
+struct db_frame {
+  int page_id;
+  int pin_count;
+  int is_dirty;
+  char data[PAGE_SIZE];
+};
+
+struct page_table_entry {
+  int frame_id;
+  int page_id;
+};
+
+
+struct db_buffer_pool {
+  struct db_frame *frames;
+  int pool_size;
+
+  struct page_table_entry *page_entry;
+
+  int *free_list;
+  int free_count;
+
+  int *lru_list;
+  int lru_size;
+
+  // pthread_mutex_t latch;
+
+  int next_page_id;
+  int fd;
+};
+
+int find_victim(struct db_buffer_pool *bp);
+void pt_insert(struct db_buffer_pool *bp, int page_id, int frame_id);
+void unpin_page(struct db_buffer_pool *bp, int page_id, int is_dirty);
+struct db_buffer_pool *buffer_init(int pool_size, int fd);
+struct db_frame *fetch_page(struct db_buffer_pool *bp, int page_id);
+
+
+#endif
